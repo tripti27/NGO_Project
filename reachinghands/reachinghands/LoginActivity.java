c1f7ee3f19e74c593443a305802d7cf3c21d4a09
Varsha Jambunathan <jvarsha621@gmail.com>
2017-07-08 16:47:18 +0530

    Login Button 2

--- reachinghands/reachinghands/LoginActivity.java
+++ reachinghands/reachinghands/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.cfg.reachinghands;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -22,7 +24,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.login:
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                Toast.makeText(this, "Log In successful!", Toast.LENGTH_SHORT).show();
+                break;
             case R.id.signup:
+                
         }
     }
 }
