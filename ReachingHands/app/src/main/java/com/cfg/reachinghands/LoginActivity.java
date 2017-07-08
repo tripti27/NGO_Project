7df57306f6c211bf33ffd9ecb83afa95d2d37732
Varsha Jambunathan <jvarsha621@gmail.com>
2017-07-08 16:25:09 +0530

    Auth 2

--- ReachingHands/app/src/main/java/com/cfg/reachinghands/LoginActivity.java
+++ ReachingHands/app/src/main/java/com/cfg/reachinghands/LoginActivity.java
@@ -2,13 +2,19 @@ package com.cfg.reachinghands;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
-    
+    Button mLogin, mSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportActionBar().setTitle("Reaching Hands");
+        getSupportActionBar().setSubtitle("Login");
+
+        
     }
 }
