a19eb4a8d7a2dad097ff7d999842ec9d947596dd
Varsha Jambunathan <jvarsha621@gmail.com>
2017-07-09 04:11:40 +0530

    Intermediate commit

--- ReachingHands/app/src/main/java/com/cfg/reachinghands/AttendanceActivity.java
+++ ReachingHands/app/src/main/java/com/cfg/reachinghands/AttendanceActivity.java
@@ -7,8 +7,17 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.cfg.reachinghands.Adapters.AttendenceAdapter;
 import com.cfg.reachinghands.Beans.Staff;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -18,6 +27,7 @@ public class AttendanceActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager mAttendenceLayoutManager;
     private RecyclerView.Adapter mAttendenceAdapter;
     private ArrayList<Staff> mStaffList;
+    ProgressDialog loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,7 @@ public class AttendanceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_attendance);
 
         getSupportActionBar().setTitle("Mark Attendance");
+        getData();
 
         //Staff RecyclerView
         mAttendenceView = (RecyclerView) findViewById(R.id.attendanceview);
@@ -34,20 +45,22 @@ public class AttendanceActivity extends AppCompatActivity {
         mAttendenceAdapter =  new AttendenceAdapter(mStaffList);
         mAttendenceView.setAdapter(mAttendenceAdapter);
 
+        Staff staff = new Staff("-", "-", "-", "-");
         mStaffList = new ArrayList<>();
+        mStaffList.add(staff);
         mStaffList.add(new Staff("2", "3", "4", "5"));
 
     }
-/*
+
     private void getData() {
-        String id = editTextId.getText().toString().trim();
+        String id = "1"; //editTextId.getText().toString().trim();
         if (id.equals("")) {
             Toast.makeText(this, "Please enter an id", Toast.LENGTH_LONG).show();
             return;
         }
         loading = ProgressDialog.show(this,"Please wait...","Fetching...",false,false);
 
-        String url = Config.DATA_URL+editTextId.getText().toString().trim();
+        String url = Config.DATA_URL;         //editTextId.getText().toString().trim();
 
         StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
             @Override
@@ -59,12 +72,32 @@ public class AttendanceActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this,error.getMessage().toString(),Toast.LENGTH_LONG).show();
+                        Toast.makeText(AttendanceActivity.this,error.getMessage().toString(),Toast.LENGTH_LONG).show();
                     }
                 });
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
     }
-    */
+
+    private void showJSON(String response) {
+        try {
+            JSONObject root = new JSONObject(response);
+            JSONArray array = root.getJSONArray("");
+
+            for(int i = 0; i < array.length(); i++) {
+                JSONObject each = array.getJSONObject(i);
+                String name = each.getString("name");
+                String date = each.getString("date");
+                String intime = each.getString("in_time");
+                String outtime = each.getString("out_time");
+                Staff staff = new Staff(name, date, intime, outtime);
+                mStaffList.add(staff);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
---
+++ ReachingHands/app/src/main/java/com/cfg/reachinghands/Config.java
@@ -0,0 +1,10 @@
+package com.cfg.reachinghands;
+
+/**
+ * Created by Varsha on 08-07-2017.
+ */
+
+public class Config {
+    public static final String DATA_URL = "http://127.0.0.1/jpm";
+
+}
