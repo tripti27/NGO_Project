91c28b6df1320dd68add4f4c5127696a4c9bea0f
Tripti Tanvi <tripti.tanvi@gmail.com>
2017-07-09 06:36:00 +0530

    Delete sattend.php

--- sattend.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("db.php");
-$Name=$_POST['Name'];
-$intime=$_POST['datetime1'];
-$outtime=$_POST['datetime2'];
-$query=mysql_query("INSERT INTO sattend (Name,intime,outtime) VALUES ('$Name','$intime','$outtime')");
-echo "<p>Record inserted</p>";
-?>
-<a href="attend2.php">GO BACK</a>
